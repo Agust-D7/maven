1324b251240d05214d6c5759059ea3dd8a4fb94f
Agust-D7 <minglosssuga80@gmail.com>
2025-06-09 10:53:16 +0530

    Add files via upload

---
+++ prgm2Recap/src/main/java/nehas_devops/prgm2Recap/App.java
@@ -0,0 +1,13 @@
+package nehas_devops.prgm2Recap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
